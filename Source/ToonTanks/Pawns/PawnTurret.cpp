93e34ded7ca6a72ee4dcaa07f35096800f98c434
Salim <salimp2009@gmail.com>
2020-09-02 00:23:06 +0300

    ADDED PawnTurret child class APawnBase added functionality to chek Player is in range added member variabie FireRange and editable for instance and BluePrint changed the BP_ PawnTurret basE FROM  APawnBase TO  APawnTurret TODO; add Fire function;

--- Source/ToonTanks/Pawns/PawnTurret.cpp
+++ Source/ToonTanks/Pawns/PawnTurret.cpp
@@ -2,4 +2,52 @@
 
 
 #include "PawnTurret.h"
+#include "PawnTank.h"
+#include "Kismet/GamePlayStatics.h"
 
+void APawnTurret::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	// Alternative way;
+	//GetWorldTimerManager();
+	GetWorld()->GetTimerManager().SetTimer(FireRateTimerHandle, this, &APawnTurret::CheckFireCondition, FireRate, true);
+
+	// To avoid expensive GamePlayStatics check, get the PlayerPawn casted to APawnTank
+	// because this is a single Player game with the APawnTank being the Player
+	PlayerPawn = Cast<APawnTank>(UGameplayStatics::GetPlayerPawn(this, 0));
+}
+
+// Called every frame
+void APawnTurret::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void APawnTurret::CheckFireCondition()
+{
+	// if Player == null || isDead then NO FIRE
+	if (!PlayerPawn)
+	{
+		return;
+	}
+
+	// if Player is range then Fire()
+	if (ReturnDistanceToPlayer() <= FireRange)
+	{
+		// Fire()
+		UE_LOG(LogTemp, Warning, TEXT("Fire Condition Checked: FIRE..."));
+	}
+	
+}
+
+float APawnTurret::ReturnDistanceToPlayer()
+{
+	if (!PlayerPawn)
+	{
+		return 0.0f;
+	}
+
+	return FVector::Dist(PlayerPawn->GetActorLocation(), this->GetActorLocation());
+}
--- Source/ToonTanks/Pawns/PawnTurret.h
+++ Source/ToonTanks/Pawns/PawnTurret.h
@@ -3,15 +3,35 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Pawns/PawnBase.h"
+#include "PawnBase.h"
 #include "PawnTurret.generated.h"
 
-/**
- * 
- */
+class APawnTank;
+
+
 UCLASS()
 class TOONTANKS_API APawnTurret : public APawnBase
 {
 	GENERATED_BODY()
 	
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Combat", meta = (AllowPrivateAccess = "true"))
+	float FireRate = 2.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Combat", meta = (AllowPrivateAccess = "true"))
+	float FireRange = 500.0f;
+
+	FTimerHandle FireRateTimerHandle;
+	APawnTank* PlayerPawn;
+
+	void CheckFireCondition();
+	float ReturnDistanceToPlayer();
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
 };
